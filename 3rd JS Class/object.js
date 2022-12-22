19a2d0dbb220803c1ae3fde9418fcd076d4d7c21
Mehrin-Anannya <anannyamehrin@gmail.com>
2022-12-22 15:42:05 +0600

    Created a new file for object

---
+++ 3rd JS Class/object.js
@@ -0,0 +1,66 @@
+// //Object
+
+// //student variables without object creation
+// var name = "Mehrin Anannya";
+// var roll = 12345;
+// var year = "1st";
+// var courses = ["Operating System", "Data Structure", "Algorithm"];
+
+// //student variables with object creation
+// var student1 = {
+//     name : "Mehrin Anannya",
+//     roll : 12345,
+//     year : "1st",
+//     courses : ["Operating System", "Data Structure", "Algorithm"]
+// }
+
+// var student1 = {
+//     name : "Karim",
+//     roll : 12345,
+//     year : "1st",
+//     courses : ["Operating System", "Data Structure", "Algorithm"]
+// }
+
+// var student2 = {
+//     name : "Rahim",
+//     roll : 6789,
+//     year : "2nd",
+//     courses : ["Operating System", "Data Structure", "Algorithm"]
+// }
+
+// var student3 = {
+//     name : "Rahim",
+//     roll : 4566,
+//     year : "3rd",
+//     courses : ["Operating System", "Data Structure", "Algorithm"]
+// }
+
+// console.log(student1.courses);
+// console.log(student2.name);
+// console.log(student3.roll);
+
+
+// //constructor
+
+// function Student(name, age, roll, year, courses){
+//     this.name = name;
+//     this.age = age;
+//     this.roll = roll;
+//     this.courses = courses;
+
+
+//     this.display = function(){
+//         console.log(this.name);
+//         console.log(this.roll);
+//         console.log(this.year);
+//         console.log(this.courses);
+//     }
+// }
+
+// var student5  = new Student("AS", 123, "1st", ["Operating System", "Data Structure", "Algorithm"]);
+// console.log(student5.name);
+// console.log(student5.roll);
+
+
+// //adding function in the object
+// student5.display();
